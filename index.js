7e8cafe67e6e63b5769760a75336db5fe824e677
famipiji <fahmihafizi40@gmail.com>
2023-03-28 22:12:26 +0800

    exercise variable

---
+++ index.js
@@ -0,0 +1,33 @@
+//let data1 = 1
+//let data2 = "1"
+//console.log(data1, typeof data1)
+//console.log(data2, typeof data2)
+
+//let data = [ "daus", 4, true]
+//console.log( data)
+
+//let data = [ "daus", 4, true]
+//console.log( data(2), typeof data[2])\
+
+//object
+let user = {
+    name: "fahmi",
+    faculty: "fkekk",
+    email: ["hasan@gmail.com" , "dauskepam"]
+}
+let user2 = {
+    name: "hasan",
+    faculty: "fkt",
+    phone: {
+        mobile: "1234",
+        office: "8799"
+    }
+}
+console.log( user.name)
+console.log(user.faculty)
+console.log(user.email[1])//<-since ada 2 email, guna index
+
+console.log(user2.name)
+console.log(user2.faculty)
+
+console.log(user2.phone)
